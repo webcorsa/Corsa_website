c71a8e83946d8320e30e641e2be5202773bfb88e
webcorsa <yatish@corsa.com>
2014-09-22 23:27:54 -0400

    Initial Creation

---
+++ Corsa_Technology/Home_files/Home.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20140922
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Home_files/HomeMoz.css')
+detectBrowser();adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
